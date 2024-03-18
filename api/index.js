1c1df86231e25a04b1436cdf07cb573e809bab51
Prem <89295807+greeenprem@users.noreply.github.com>
2024-03-18 20:01:28 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -19,23 +19,21 @@ const burp0_cookies = {
     "__Secure-next-auth.session-token": "a6f35133-dea1-48d2-94c3-c2ea42394b29"
 };
 
-app.get('/api', async (req, res) => {
+app.get('/api', (req, res) => {
     const query_url = req.query.query;
     console.log(query_url);
     if (!query_url) {
         return res.status(400).send('No query URL provided');
     }
 
-    try {
-        const response = await axios.get(query_url, { headers: { Cookie: Object.keys(burp0_cookies).map(key => `${key}=${burp0_cookies[key]}`).join('; ') } });
-        res.set('content-type', response.headers['content-type']);
-        res.send(response.data);
-    } catch (error) {
-        console.error(error);
-        const responseData = error.response ? error.response.data : error.message;
-        console.log(responseData)
-        res.status(error.response?.status || 500).send(JSON.stringify(responseData));
-    }
+    axios.get(query_url, { headers: { Cookie: Object.keys(burp0_cookies).map(key => `${key}=${burp0_cookies[key]}`).join('; ') } })
+        .then(response => {
+            res.send(response.data);
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(error.response?.status || 500).send(error.response?.data || error.message);
+        });
 });
 
 
